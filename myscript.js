986d79b56880ec2bf530b1bc5125c66e48f6919e
AgneseBettio <bettio.agnese@gmail.com>
2021-04-07 16:22:06 +0200

    esercizio completato

--- myscript.js
+++ myscript.js
@@ -1,5 +1,8 @@
-var nome = prompt(inserisci il tuo nome)
-var cognome = prompt(inserisci il tuo cognome)
-var colorePrefe = prompt(inserisci il tuo colore preferito)
-var annoCorrente = "21"
+var nome = prompt("inserisci il tuo nome");
+var cognome = prompt("inserisci il tuo cognome");
+var colorePrefe = prompt("inserisci il tuo colore preferito");
+var annoCorrente = "21";
 
+var passwordGen = (nome + cognome + colorePrefe + annoCorrente);
+console.log(passwordGen);
+document.getElementById("password-generated").innerHTML= passwordGen;
